131814cc936804d224ed9a580cedb80b4f315885
MyckChamorro <mschamorro@espe.edu.ec>
2025-06-10 10:19:11 -0500

    Front

--- server/server.js
+++ server/server.js
@@ -19,7 +19,19 @@ db.connect((err) => {
 
 const PORT = 3000;
 
+
 const server = http.createServer((req, res) => {
+    
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+
+    if (req.method === 'OPTIONS') {
+        res.writeHead(204);
+        res.end();
+        return;
+    }
+
     // Servir archivos estáticos
     if (req.method === 'GET' && req.url.startsWith('/public/')) {
         const filePath = path.join(__dirname, req.url);
